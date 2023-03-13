40d3b4c000c72d1f8b5c781779751c3000b3f257
amal2806 <amalbabu2806@gmail.com>
2023-03-13 00:11:38 -0930

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+// var x=20 ;
+// {
+//     let x=10;
+//     console.log(x);
+// }
+// console.log(x);
+// const pi=3.14;
+// console.log(pi);
+// var sum=5+10;
+// console.log(sum);
+// var subraction=10-2
+// console.log(subraction);
+// var division=10/2;
+// console.log(division);
+// var multi=5*10;
+// console.log(multi);
+// var mod=10%2;
+// console.log(mod);
+// document.write("sum ="+ sum);
+// document.write("subraction ="+ subraction);
+// document.write("division ="+ division);
+// document.write("multi ="+ multi);
+// document.write(`sum is ${sum}`)
+
+// var students={
+//     name:"Adarsh",
+//     age:19,
+//     place:"thrissur"
+// }
+// console.log(students);
+// console.log(students.name);
+// console.log(students.age);
+
+// var cars={
+//     name:"nissan",
+//     model:"gtr",
+//     color:"black",
+//     price:4000000
+// };
+// console.log(cars.model);
+
+// var fruits=["apple","banana","orange",10];
+// fruits[2]="grapes";
+// fruits.push("plum");
+// fruits.unshift("orange");
+// fruits.pop();
+// fruits.shift();
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[3]);
+// console.log(fruits.length);
+
+// var students=[
+//     {
+//        name:"Adarsh",
+//        age:19,
+//         place:"thrissur"
+//     },
+//     {
+//        name:"Adhi",
+//        age:20,
+//         place:"trikkur"
+//     }
+// ];
+    // console.log(students[1]);
+    // function add(a,b){
+    //     var c=a+b;
+    //     console.log(c);
+    // }
+    // add(2,8)
+    // function mult(a,b){
+    //     var c=a*b;
+    //     console.log(c);
+    // }
+    // mult(4,5)
+
+    // const addition =(a,b)=>{
+    //     var c=a+b;
+    //     console.log(c);
+    // }
+    // addition(1,7)
+
+    // const multi =(a,b)=>{
+    //     var c=a*b;
+    //     console.log(c);
+    // }
+    // multi(3,2)
+    // var x=20;
+    // if(x>20){
+    //     console.log("good");
+    // }
+    // else
+    // console.log("bad");
+    // for(var i=1;i<=5;i++)
+    // {
+    //     console.log(i);
+    // }
+
+    var fruits=["apple","banana","orange","mango","guava","litchi","grapes"];
+    // for(i=0;i<fruits.length;i++)
+    // console.log(fruits[i]);
+
+    let i=0;
+    do{
+        console.log(fruits[i]);
+        i++;
+    }while(i<fruits.length)
